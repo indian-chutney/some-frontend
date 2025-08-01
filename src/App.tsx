2c95bcdff74cf26e87f1ef049269698211c45ac5
Zubair <127671949+indian-chutney@users.noreply.github.com>
2025-08-01 10:54:47 +0530

    Convert App.jsx to App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface AppProps {
+  // Define props types here
+}
+
+const App: React.FC<AppProps> = (props) => {
+  // Define state with types if necessary
+  return (
+    <div>
+      {/* Component content */}
+    </div>
+  );
+};
+
+export default App;
